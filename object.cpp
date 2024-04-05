fcad5c0aa80c10dcc4f7e5acc7bfae6000b97c43
Filip Edberger <filed881@student.liu.se>
2024-04-05 23:22:41 +0200

    Added functions to object

--- object.cpp
+++ object.cpp
@@ -7,9 +7,9 @@ Object::Object() {
     return;
 }
 
-void Object::create_model(std::string filename, int x, int y, int z, float sc) {
+void Object::create_model(std::string filename, vec3 pos, float sc) {
     model = LoadModel(filename.c_str());
-    position = vec3(x, y, z);
+    position = pos;
     scale = sc;
     update_model2world();
 
@@ -23,3 +23,25 @@ void Object::display(GLuint program) {
     upload2shader(program);
     DrawModel(model, program, "in_Position", "in_Normal", "in_TexCoord");
 }
+
+void Object::translate(vec3 translation) {
+        position = position + translation;
+        update_model2world();
+    }
+
+void Object::move(vec3 coordinates) {
+    position = coordinates;
+    update_model2world();
+}
+
+void Object::rotate(float angle, vec3 axis) {
+    model2world = model2world * ArbRotate(axis, angle);
+}
+
+void Object::update_model2world() {
+        model2world = T(position.x, position.y, position.z) * S(scale);
+    }
+
+void Object::upload2shader(GLuint program) {
+        glUniformMatrix4fv(glGetUniformLocation(program, "mdlMatrix"), 1, GL_TRUE, model2world.m);
+    }
--- object.h
+++ object.h
@@ -10,8 +10,10 @@ class Object {
     Object();
     virtual void update();
     virtual void display(GLuint program);
-    virtual void create_model(std::string filename, int x = 0, int y = 0, int z = 0, float sc = 1);
-
+    virtual void create_model(std::string filename, vec3 pos = vec3(0,0,0), float sc = 1);
+    virtual void translate(vec3 translation);
+    virtual void move(vec3 coordinates);
+    virtual void rotate(float angle, vec3 axis);
     virtual ~Object() = default;
 
     protected:
@@ -20,27 +22,8 @@ class Object {
     vec3 position;
     float scale;
 
-    virtual void update_model2world() {
-        model2world = T(position.x, position.y, position.z) * S(scale);
-    }
-
-    virtual void upload2shader(GLuint program) {
-        glUniformMatrix4fv(glGetUniformLocation(program, "mdlMatrix"), 1, GL_TRUE, model2world.m);
-    }
-
-    virtual void translate(vec3 translation) {
-        position = position + translation;
-        update_model2world();
-    }
-
-    virtual void move(vec3 coordinates) {
-        position = coordinates;
-        update_model2world();
-    }
-
-
-
-
+    void update_model2world();
+    void upload2shader(GLuint program);
 
 };
 
