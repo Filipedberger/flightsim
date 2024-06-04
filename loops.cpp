4b77a521cb98b25563e51555a0e214962498adc1
Victor Eriksson <vicer124@student.liu.se>
2024-06-04 15:40:48 +0200

    Fixed bug where hoop would spawn under the terrain

--- loops.cpp
+++ loops.cpp
@@ -21,12 +21,14 @@ Loops::Loops(Json::Value settings, vec3 cameraPosition)
     allowedSpace_y_high = settings["allowedSpace_y_high"].asFloat();
 
     loopPos = cameraPosition + offset_distance;
-    
-    if (abs(loopPos.y) < allowedSpace_y_low) {
+
+    if (loopPos.y < allowedSpace_y_low)
+    {
         std::cout << "New loop position is outside the LOW allowed space\n";
         loopPos.y = allowedSpace_y_low;
     }
-    if (abs(loopPos.y) > allowedSpace_y_high) {
+    if (loopPos.y > allowedSpace_y_high)
+    {
         std::cout << "New loop position is outside the HIGH allowed space\n";
         loopPos.y = allowedSpace_y_high;
     }
@@ -37,7 +39,6 @@ Loops::Loops(Json::Value settings, vec3 cameraPosition)
     waypoint_scale = S(waypointer_sc, waypointer_sc, waypointer_sc);
 
     calculate_radius();
-    
 }
 
 void Loops::update(int time_elapsed, vec3 cameraPosition, vec3 lookAtPoint, std::map<char, bool> keys_pressed)
@@ -49,21 +50,20 @@ void Loops::update(int time_elapsed, vec3 cameraPosition, vec3 lookAtPoint, std:
     waypoint_rotation = ArbRotate(rotation_axis, rotation_angle);
     waypoint_translation = T(lookAtPoint.x, lookAtPoint.y, lookAtPoint.z);
 
-
-
     float distanceToCenter = length(lookAtPoint - loopPos);
     bool isInsideThisFrame = distanceToCenter < radius;
 
-    if (!wasInsideLastFrame && isInsideThisFrame) {
+    if (!wasInsideLastFrame && isInsideThisFrame)
+    {
         std::cout << "Airplane has entered the torus.\n";
         create_new_loop(lookAtPoint);
-    } 
+    }
 
     wasInsideLastFrame = isInsideThisFrame;
     return;
 }
 
-void Loops::display(const GLuint& program, const mat4& world2view, const mat4& projection, vec3 light_int)
+void Loops::display(const GLuint &program, const mat4 &world2view, const mat4 &projection, vec3 light_int)
 {
     Object::display(program, world2view, projection);
 
@@ -72,10 +72,10 @@ void Loops::display(const GLuint& program, const mat4& world2view, const mat4& p
     glUniformMatrix4fv(glGetUniformLocation(program, "rotationMatrix"), 1, GL_TRUE, waypoint_rotation.m);
     glUniformMatrix4fv(glGetUniformLocation(program, "scaleMatrix"), 1, GL_TRUE, waypoint_scale.m);
     DrawModel(waypoint_model, program, "in_Position", "in_Normal", NULL);
-
 }
 
-float Loops::length(vec3 v) {
+float Loops::length(vec3 v)
+{
     return sqrt(v.x * v.x + v.y * v.y + v.z * v.z);
 }
 
@@ -85,25 +85,27 @@ void Loops::create_new_loop(vec3 cameraPosition)
     std::random_device rd;
     std::mt19937 gen(rd());
     std::uniform_real_distribution<> dis(0, 1);
-    float azimuth = 2 * M_PI * dis(gen); // Random angle in [0, 2*pi]
+    float azimuth = 2 * M_PI * dis(gen);      // Random angle in [0, 2*pi]
     float elevation = acos(2 * dis(gen) - 1); // Random angle in [0, pi]
 
     // Convert spherical coordinates to Cartesian coordinates.
     vec3 direction(sin(elevation) * cos(azimuth), sin(elevation) * sin(azimuth), cos(elevation));
 
     // Scale by the desired distance and add to the airplane's position to get the loop's position.
-    
+
     vec3 newLoopPos = cameraPosition + direction * offset_distance;
 
-    if (abs(newLoopPos.y) < allowedSpace_y_low) {
+    if (newLoopPos.y < allowedSpace_y_low)
+    {
         std::cout << "New loop position is outside the LOW allowed space\n";
         newLoopPos.y = allowedSpace_y_low;
     }
-    if (abs(newLoopPos.y) > allowedSpace_y_high) {
+    if (newLoopPos.y > allowedSpace_y_high)
+    {
         std::cout << "New loop position is outside the HIGH allowed space\n";
         newLoopPos.y = allowedSpace_y_high;
     }
-   
+
     // The new position is above the terrain. Create a new loop at this position.
     loopPos = newLoopPos;
 
@@ -129,7 +131,6 @@ void Loops::calculate_radius()
 
     radius = sqrt(radius) * sc; // OBS SHOULD CHANGE THIS WHEN SCALING IS CHANGED, NOT IMPLEMENTED YET
 }
-
 
 Loops::~Loops()
 {
